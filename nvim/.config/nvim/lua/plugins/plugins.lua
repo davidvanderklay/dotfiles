e3834604462e1dff1e0ced3e27c599c3ee341c56
David Vanderklay <davidvanderklay@gmail.com>
2025-05-30 00:05:04 -0500

    add cisco vpn, upgrade nvim pkg versions

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -9,4 +9,10 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
 }
