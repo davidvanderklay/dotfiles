2eb1fdcc78a1f9fd86f0acc204652bb2572c3e1b
David Vanderklay <davidvanderklay@gmail.com>
2025-11-24 00:29:16 -0600

    make lazy a little lighter

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -8,10 +8,4 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  },
 }
