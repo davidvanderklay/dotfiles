081d39155c741afeff5417f6fde91b2a2ce0ae1d
davidvanderklay <davidvanderklay@gmail.com>
2025-12-07 03:12:10 -0600

    things now work

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -18,6 +18,8 @@
 	nixosConfigurations = {
 		nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+
 			modules = [
 				./configuration.nix
 				home-manager.nixosModules.home-manager
