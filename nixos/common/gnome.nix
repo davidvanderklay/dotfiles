cdf1f0d5fa19296d59b026b81cfb1bf44885d44c
davidvanderklay <davidvanderklay@gmail.com>
2025-12-07 22:07:47 -0600

    use correct zen name

--- nixos/common/gnome.nix
+++ nixos/common/gnome.nix
@@ -19,11 +19,11 @@
   xdg.mimeApps = {
       enable = true;
       defaultApplications = {
-          "text/html" = "zen.desktop";
-          "x-scheme-handler/http" = "zen.desktop";
-          "x-scheme-handler/https" = "zen.desktop";
-          "x-scheme-handler/about" = "zen.desktop";
-          "x-scheme-handler/unknown" = "zen.desktop";
+          "text/html" = "zen-beta.desktop";
+          "x-scheme-handler/http" = "zen-beta.desktop";
+          "x-scheme-handler/https" = "zen-beta.desktop";
+          "x-scheme-handler/about" = "zen-beta.desktop";
+          "x-scheme-handler/unknown" = "zen-beta.desktop";
         };
     };
 
