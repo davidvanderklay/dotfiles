cb1848116315fa927d44ebb1eebcd51180e72e08
davidvanderklay <davidvanderklay@gmail.com>
2025-12-08 21:31:51 -0600

    harpoon keybinds

--- nixos/common/nixvim.nix
+++ nixos/common/nixvim.nix
@@ -414,6 +414,43 @@
         action.__raw = "function() require('harpoon'):list():add() end";
         options.desc = "Harpoon File (Add)";
       }
+      {
+        mode = "n";
+        key = "<leader>1";
+        action.__raw = "function() require('harpoon'):list():select(1) end";
+        options.desc = "Harpoon File 1";
+      }
+      {
+        mode = "n";
+        key = "<leader>2";
+        action.__raw = "function() require('harpoon'):list():select(2) end";
+        options.desc = "Harpoon File 2";
+      }
+      {
+        mode = "n";
+        key = "<leader>3";
+        action.__raw = "function() require('harpoon'):list():select(3) end";
+        options.desc = "Harpoon File 3";
+      }
+      {
+        mode = "n";
+        key = "<leader>4";
+        action.__raw = "function() require('harpoon'):list():select(4) end";
+        options.desc = "Harpoon File 4";
+      }
+      {
+        mode = "n";
+        key = "<leader>5";
+        action.__raw = "function() require('harpoon'):list():select(5) end";
+        options.desc = "Harpoon File 5";
+      }
+      {
+        mode = "n";
+        key = "<leader>6";
+        action.__raw = "function() require('harpoon'):list():select(5) end";
+        options.desc = "Harpoon File 6";
+      }
+
     ];
 
     # --- 5. EXTRA PACKAGES (Environment) ---
