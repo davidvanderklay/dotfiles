71cf157b35dbf9b5a5c5a7d9d7eae7ac84e25144
davidvanderklay <davidvanderklay@gmail.com>
2025-12-08 21:45:20 -0600

    move line up down keybinds from lazyvim

--- nixos/common/nixvim.nix
+++ nixos/common/nixvim.nix
@@ -337,6 +337,43 @@
         options.desc = "Lazygit";
       }
 
+      {
+        mode = "n";
+        key = "<A-j>";
+        action = "<cmd>m .+1<cr>==";
+        options.desc = "Move Down";
+      }
+      {
+        mode = "n";
+        key = "<A-k>";
+        action = "<cmd>m .-2<cr>==";
+        options.desc = "Move Up";
+      }
+      {
+        mode = "i";
+        key = "<A-j>";
+        action = "<esc><cmd>m .+1<cr>==gi";
+        options.desc = "Move Down";
+      }
+      {
+        mode = "i";
+        key = "<A-k>";
+        action = "<esc><cmd>m .-2<cr>==gi";
+        options.desc = "Move Up";
+      }
+      {
+        mode = "v";
+        key = "<A-j>";
+        action = ":m '>+1<cr>gv=gv";
+        options.desc = "Move Down";
+      }
+      {
+        mode = "v";
+        key = "<A-k>";
+        action = ":m '<-2<cr>gv=gv";
+        options.desc = "Move Up";
+      }
+
       # Window Nav
       {
         mode = "n";
