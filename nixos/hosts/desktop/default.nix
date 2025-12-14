97e28f033353ed2521a38810c055a621b71a60fc
David Vanderklay <davidvanderklay@gmail.com>
2025-12-13 21:50:07 -0600

    install obs on desktop

--- nixos/hosts/desktop/default.nix
+++ nixos/hosts/desktop/default.nix
@@ -74,4 +74,24 @@ in
     osu-lazer-bin # This installs the actual game assets
     pavucontrol # Audio control
   ];
+
+  programs.obs-studio = {
+    enable = true;
+
+    # optional Nvidia hardware acceleration
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 }
