e4d0cb90ba8cc2d1e2cb5a1164531142e3aa30ea
David Vanderklay <davidvanderklay@gmail.com>
2025-12-15 23:45:49 -0600

    more driver stuff

--- nixos/common/configuration.nix
+++ nixos/common/configuration.nix
@@ -66,7 +66,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.brlaser ];
+  services.printing.drivers = [
+    pkgs.brlaser
+    pkgs.brgenml1lpr
+    pkgs.brgenml1cupswrapper
+  ];
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
