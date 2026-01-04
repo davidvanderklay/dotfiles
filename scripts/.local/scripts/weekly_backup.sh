532ce41254b00576e8a7ace111b5ddda3e492dbb
yes <yes@gmail.com>
2026-01-04 04:58:29 +0000

    non-dry script, correct directory please work

--- scripts/.local/scripts/weekly_backup.sh
+++ scripts/.local/scripts/weekly_backup.sh
@@ -2,7 +2,7 @@
 
 # 1. Add the trailing slash to the source
 SOURCE="/sambashare/"
-DEST="van@100.108.108.93:/home/van/sambashare/"
+DEST="van@100.108.108.93:/home/van/sambashare/sambashare/"
 LOGFILE="/home/van/backup_log.log"
 
 # 2. SAFETY CHECK: Only run if the source directory is not empty
@@ -16,6 +16,6 @@ echo "--- Backup started at $(date) ---" >>$LOGFILE
 
 # 3. Add --dry-run first to test safely!
 # REMOVE the "-n" once you are 100% sure the log looks correct.
-rsync -avzH -n --delete $SOURCE $DEST >>$LOGFILE 2>&1
+rsync -avzH --delete $SOURCE $DEST >>$LOGFILE 2>&1
 
 echo "--- Backup finished at $(date) ---" >>$LOGFILE
