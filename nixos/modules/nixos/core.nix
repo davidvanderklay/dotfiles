b9efd0cd0c0d24315814d75062da210ffcb5ffa2
David Vanderklay <davidvanderklay@gmail.com>
2026-07-19 17:45:56 -0500

    Remove insecure Electron pin
    - Drop `electron-40.10.5` from `permittedInsecurePackages`
    - Keep NixOS core config aligned with current nixpkgs

--- nixos/modules/nixos/core.nix
+++ nixos/modules/nixos/core.nix
@@ -134,8 +134,6 @@ in
     };
 
     nixpkgs.config.allowUnfree = true;
-    # vesktop pins EOL electron until nixpkgs bumps it
-    nixpkgs.config.permittedInsecurePackages = [ "electron-40.10.5" ];
 
     environment.variables.EDITOR = "nvim";
 
